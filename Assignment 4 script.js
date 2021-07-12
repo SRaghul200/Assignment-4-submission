f48611716009c3fd0f291dbd0e31e2c8206240cd
SRaghul200 <85299628+SRaghul200@users.noreply.github.com>
2021-07-12 13:54:01 +0530

    assigment 4 submitted

---
+++ Assignment 4 script.js
@@ -0,0 +1,12 @@
+function Handle() {
+     var name = document.getElementById("inp")
+
+     var data = name.value;
+
+     var dart = document.getElementById("head")
+      
+     dart.innerHTML ="My Name is " + data;
+
+
+    //  alert("My Name is" +" "+ data);
+ }
